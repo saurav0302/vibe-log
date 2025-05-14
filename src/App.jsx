f18fc12bc4aa982eb66dd96a37fd95411369620f
Saurav Kosti <kosti.saurav@gmail.com>
2025-05-14 22:27:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+console.log(import.meta.env.VITE_TEST_ENV);
+  return (
+    <>
+      <p>This is my Blog app starting bro !!</p>
+    </>
+  )
+}
+
+export default App
